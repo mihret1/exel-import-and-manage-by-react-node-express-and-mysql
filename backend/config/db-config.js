6ef99b6831a53366989214e3e47fd529ecda932e
mihret1 <mihlet2@gmail.com>
2023-06-09 15:10:57 +0300

    connect myql database by sequelize the backend

---
+++ backend/config/db-config.js
@@ -0,0 +1,9 @@
+module.exports={
+    HOST:'localhost',
+    USER:'root',
+    PASSWORD:'your_new_password',
+    DATABASE:'my-exel-app',
+    DIALECT:'mysql'
+
+
+}
--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,18 @@ const express=require('express')
 const app=express()
 const PORT=6000
 
+const db=require('./models')
+
+// (async ()=>{
+//     await db.sequelize.sync()
+// })();
+
+const dbb=async()=>{
+    await db.sequelize.sync()
+
+}
+
+dbb()
+
 
 app.listen(PORT,()=>{console.log(`server is  runing on port ${PORT}`)})
